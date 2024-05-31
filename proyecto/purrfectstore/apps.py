efded8583da27bbfa27f94354c808058b3131270
fl-oneto <fl.oneto@duocuc.cl>
2024-05-31 01:06:56 -0400

    se agrega modelo de datos

---
+++ proyecto/purrfectstore/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PurrfectstoreConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'purrfectstore'
---
+++ proyecto/purrfectstore/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=200, unique=True)
+    descripcion = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
+
+class Producto(models.Model):
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, related_name='productos')
+    nombre = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    disponible = models.BooleanField(default=True)
+    creado_en = models.DateTimeField(auto_now_add=True)
+    actualizado_en = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nombre
