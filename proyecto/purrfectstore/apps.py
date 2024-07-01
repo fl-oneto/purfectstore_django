10db0b0c7f53f0cdc8adf46926c35caeaee9cfda
Guonejo <ma.carvachos@duocuc.cl>
2024-07-01 05:05:18 -0400

    ns

--- proyecto/purrfectstore/apps.py
+++ proyecto/purrfectstore/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
 
 
+
 class PurrfectstoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'purrfectstore'
+
+def ready(self):
+        import purrfectstore.signals  
